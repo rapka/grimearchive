22d1a02e2d88e66e1cf451ef829d800808763c84
Richard Adjogah <richard.adjogah@gmail.com>
2015-01-11 12:04:33 -0500

    Added support for page selection

--- routes/mixes.js
+++ routes/mixes.js
@@ -5,33 +5,46 @@ var ObjectId = require('mongoose').Types.ObjectId;
 exports.routes = function(app) {
 	app.get('/mixes', exports.index);
 	app.get('/mix/:url', exports.view);
+	app.get('/mixes/page/:page', exports.page);
 
 
 };
 
 exports.index = function(req, res) {
-	Mix.find().sort({date: -1}).limit(40)
+	Mix.find().sort({date: -1}).limit(10)
 		.exec(function(err, mixes) {
 			if (err){
 				console.log("find error");
 				throw err;
 			}
 
-			res.render('mixes', {title: 'Grimelist', mixes: mixes});
+			res.render('mixes', {title: 'Grimelist', mixes: mixes, page: 1});
 	});
 };
 
-exports.mixes = function(req, res) {
-	res.render('mixes', {
-		title: 'Grimelist',
-		mixes:  mixes
+exports.page = function(req, res) {
+	var page = req.params.page;
+
+	if (page < 1) {
+		page = 1;
+	}
+
+	var skip = (page - 1) * 10;
+
+	Mix.find().skip(skip).sort({date: -1}).limit(10)
+		.exec(function(err, mixes) {
+			if (err){
+				console.log("find error");
+				throw err;
+			}
+
+			res.render('mixes', {title: 'Grimelist', mixes: mixes, page: page});
 	});
 };
+
 
 exports.view = function(req, res) {
 	var mix = Mix.findOne({url: req.params.url}).exec(function (err, mix){
-		console.log("mmiiixx");
-		console.log(req.params.url);
 		res.render('mix', {
 			title: 'Grimelist',
 			mix: mix
