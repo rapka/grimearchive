08a237765f892d684f1c5a39f2d5092c33e11317
Richard Adjogah <richard.adjogah@gmail.com>
2015-01-11 14:23:39 -0500

    Fixed pagination for individual mc/crew/dj listings

--- routes/search.js
+++ routes/search.js
@@ -3,46 +3,69 @@ var Mix = mongoose.model('Mix');
 var ObjectId = require('mongoose').Types.ObjectId;
 
 exports.routes = function(app) {
-	app.get('/dj/:url', exports.dj);
-	app.get('/mc/:url', exports.mc);
-	app.get('/crew/:url', exports.crew);
+	app.get('/dj/:url/page/:page', exports.dj);
+	app.get('/mc/:url/page/:page', exports.mc);
+	app.get('/crew/:url/page/:page', exports.crew);
 	app.get('/search/:term', exports.search);
 
 };
 
 exports.dj = function(req, res) {
-	Mix.find({dj: req.params.url, hidden: false}).sort({date: -1}).limit(40)
+		var page = req.params.page;
+
+	if (page < 1) {
+		page = 1;
+	}
+
+	var skip = (page - 1) * 20;
+	Mix.find({dj: req.params.url, hidden: false}).skip(skip).sort({date: -1}).limit(20)
 		.exec(function(err, mixes) {
 			if (err){
 				console.log("find error");
 				throw err;
 			}
-
-			res.render('mixes', {title: 'Grimelist', mixes: mixes});
+			var currentUrl = '/dj/' + req.params.url + '/page/';
+			res.render('mixes', {url: currentUrl, mixes: mixes, page: page});
 	});
 };
 
 exports.mc = function(req, res) {
-	Mix.find({mcs: req.params.url, hidden: false}).sort({date: -1}).limit(40)
+		var page = req.params.page;
+
+	if (page < 1) {
+		page = 1;
+	}
+
+	var skip = (page - 1) * 20;
+	Mix.find({mcs: req.params.url, hidden: false}).skip(skip).sort({date: -1}).limit(20)
 		.exec(function(err, mixes) {
 			if (err){
 				console.log("find error");
 				throw err;
 			}
 
-			res.render('mixes', {title: 'Grimelist', mixes: mixes});
+			var url = '/mc/' + req.params.url + '/page/';
+			res.render('mixes', {url: currentUrl, mixes: mixes, page: page});
 	});
 };
 
 exports.crew = function(req, res) {
-	Mix.find({crews: req.params.url, hidden: false}).sort({date: -1}).limit(40)
+	var page = req.params.page;
+
+	if (page < 1) {
+		page = 1;
+	}
+
+	var skip = (page - 1) * 20;
+	Mix.find({crews: req.params.url, hidden: false}).skip(skip).sort({date: -1}).limit(20)
 		.exec(function(err, mixes) {
 			if (err){
 				console.log("find error");
 				throw err;
 			}
 
-			res.render('mixes', {title: 'Grimelist', mixes: mixes});
+			var url = '/crew/' + req.params.url + '/page/';
+			res.render('mixes', {url: currentUrl, mixes: mixes, page: page});
 	});
 };
 
