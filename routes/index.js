04709717a407819f83d5e61b9aac766d1c3b61b0
Richard Adjogah <richard.adjogah@gmail.com>
2015-02-07 12:18:15 -0500

    Redesigned the main page to show recent & popular mixes.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
+var mongoose = require('mongoose');
+var Mix = mongoose.model('Mix');
+var ObjectId = require('mongoose').Types.ObjectId;
+
 exports.routes = function(app) {
 	app.get('/', exports.index);
 	app.get('/about', exports.about);
 };
 
 exports.index = function(req, res) {
-	res.render('index', { title: 'Grimelist' });
+	Mix.find({hidden: false}).sort({date: -1}).limit(10).exec(function(err, recent) {
+			if (err){
+				console.log("find error");
+				throw err;
+			}
+
+		Mix.find({hidden: false}).sort({downloads: -1}).limit(10).exec(function(err, popular) {
+				if (err){
+					console.log("find error");
+					throw err;
+				}
+				
+				res.render('index', { title: 'Grimelist', popular: popular, recent: recent});
+		});
+	});
+
+	
 };
 
 exports.about = function(req, res) {
