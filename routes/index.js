f68f971e16ee085ef0705b0b80e936eeb43b935e
Richard Adjogah <richard.adjogah@gmail.com>
2015-05-19 19:25:10 -0400

    Show total mix hours on homepage

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,40 @@ exports.routes = function(app) {
 };
 
 exports.index = function(req, res) {
-	Mix.find({hidden: false, mcs: [], crews: []}).sort({date: -1}).limit(5).exec(function(err, instrumentals) {
+	Mix.find({hidden: false, mcs: [], crews: []}).sort({date: -1}).limit(5).exec(function (err, instrumentals) {
 		if (err){
 			console.log("find error");
 			throw err;
 		}
 
-		Mix.find({hidden: false}).sort({date: -1}).limit(5).exec(function(err, recent) {
+		Mix.find({hidden: false}).sort({date: -1}).limit(5).exec(function (err, recent) {
 			if (err){
 				console.log("find error");
 				throw err;
 			}
 
-			Mix.find({hidden: false}).sort({downloads: -1}).limit(5).exec(function(err, popular) {
+			Mix.find({hidden: false}).sort({downloads: -1}).limit(5).exec(function (err, popular) {
 				if (err){
 					console.log("find error");
 					throw err;
 				}
-				Mix.count({hidden: false}, function(err, count) {
-					res.render('index', { title: 'The Grime Archive', popular: popular, recent: recent, count: count, instrumentals: instrumentals});
+				Mix.count({hidden: false}, function (err, count) {
+					Mix.aggregate({
+						  $group: {
+						    _id: null,
+						    total: {
+						      $sum: "$duration"
+						    }
+						  }
+						}, {
+						  $project: {
+						    _id: 0,
+						    total: 1
+						  }}, function (err, result) {
+								console.log(result[0].total);
+								res.render('index', { title: 'The Grime Archive', sum: Math.floor(result[0].total / 3600), popular: popular, recent: recent, count: count, instrumentals: instrumentals});
+							});
+					
 				}); 
 				
 			});
