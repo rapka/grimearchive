369bb23170de78e60b648d2ca725b78757dba574
Richard Adjogah <richard@webs.com>
2021-05-01 15:29:52 -0400

    Refactor upload logic

--- routes/upload.js
+++ routes/upload.js
@@ -64,6 +64,7 @@ const s3 = new AWS.S3();
 exports.routes = (app) => {
   app.get('/upload', exports.index);
   app.post('/upload', upload.single('mixUpload'), exports.add);
+  app.post('/edit', exports.edit);
   app.get('/upload/:url', exports.checkFfmpeg);
 };
 
@@ -95,11 +96,153 @@ exports.add = async (req, res) => {
   try {
     const probeData = await ffprobe(file.path);
     console.log('Got probe data', probeData.streams[0].duration);
-    // await Mix.updateOne({ file: file.name }, {
-    //   bitrate: probeData.streams[0].bit_rate / 1000,
-    //   duration: probeData.streams[0].duration,
-    // });
 
+    let mix;
+
+    // Server side check for no file selected
+    if (typeof req.files.file === 'undefined' && !req.body.edit) {
+      console.log('error: no file selected');
+      return;
+    }
+
+    console.log('adding new mix', req.files);
+
+    mix = new Mix({
+      _id: mongoose.Types.ObjectId(req.body._id),
+      title: req.body.title,
+      dj: req.body.dj,
+      file: req.files.file.name,
+      station: req.body.station,
+      bitrate: probeData.streams[0].bit_rate / 1000,
+      duration: probeData.streams[0].duration,
+    });
+
+    if (req.body.username) {
+      mix.uploader = req.body.username;
+    }
+    if (req.body.tripcode) {
+      // const crypted = crypt(req.body.tripcode, crypt.createSalt('md5'));
+      const crypted = crypto.createHash('sha256').update(req.body.tripcode).digest('hex').substring(0, 8);
+      mix.tripcode = crypted;
+    }
+
+    if (req.body.hidden) {
+      mix.hidden = req.body.hidden;
+    }
+    if (req.body.mcs) {
+      mix.mcs = req.body.mcs.split(',');
+    }
+    if (req.body.day) {
+      mix.day = parseInt(req.body.day);
+    }
+    if (req.body.month) {
+      mix.month = parseInt(req.body.month);
+    }
+    if (req.body.year) {
+      mix.year = parseInt(req.body.year);
+    }
+    if (req.body.crews) {
+      mix.crews = req.body.crews.split(',');
+    }
+    if (req.body.description) {
+      mix.description = req.body.description;
+    }
+    if (req.body.youtube) {
+      mix.youtube = req.body.youtube;
+    }
+
+    console.log('beginning tag update');
+    mix.updateTags(req.body.preserve, req.body.albumtitle);
+
+    // File written successfully, save the entry in mongo.
+    mix.save((err) => {
+      if (err) {
+        console.log('500: Could not save file entry to database.', err);
+      }
+    });
+  } catch (err) {
+    console.error('Upload processing error:', err);
+  }
+
+  if (typeof req.files.file !== 'undefined') {
+    res.send('/mix/' + req.files.file.name.split('.')[0]);
+  } else {
+    console.log('no file selected');
+    res.redirect('/upload');
+  }
+};
+
+exports.edit = async (req, res) => {
+  const file = req.file;
+  console.log('inn post EDIT endpoint', file);
+
+  try {
+    let mix;
+
+    // Server side check for no file selected
+    if (typeof req.files.file === 'undefined' && !req.body.editUrl) {
+      console.log('error: no file selected');
+      return;
+    } { // Edit mix
+      mix = {
+        hidden: !!req.body.hidden,
+      };
+
+      if (req.body.title) {
+        // Hack to clear file names
+        if (req.body.title === '0xDEADBEEF') {
+          mix.title = '';
+        } else {
+          mix.title = req.body.title;
+        }
+      }
+
+      if (req.body.dj) {
+        mix.dj = req.body.dj;
+      }
+      if (req.body.station) {
+        mix.station = req.body.station;
+      }
+
+      if (req.body.mcs) {
+        mix.mcs = req.body.mcs.split(',');
+      }
+      if (req.body.day) {
+        mix.day = parseInt(req.body.day);
+      }
+      if (req.body.month) {
+        mix.month = parseInt(req.body.month);
+      }
+      if (req.body.year) {
+        mix.year = parseInt(req.body.year);
+      }
+      if (req.body.crews) {
+        mix.crews = req.body.crews.split(',');
+      }
+      if (req.body.description) {
+        mix.description = req.body.description;
+      }
+      if (req.body.youtube) {
+        mix.youtube = req.body.youtube;
+      }
+
+      Mix.updateOne({ url: req.body.editUrl }, mix, (err) => {
+        if (err) {
+          console.log(err);
+          console.error('Error updating mix.');
+        }
+      });
+
+      Mix.findOne({ url: req.body.editUrl }).exec((err, foundMix) => {
+        if (err) {
+          console.log(err);
+          console.error('Error updating mix.');
+          return;
+        }
+
+        foundMix.updateTags(true, req.body.albumtitle);
+      });
+    }
   } catch (err) {
     console.error('Upload processing error:', err);
   }
